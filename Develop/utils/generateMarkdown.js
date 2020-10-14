178e279c06a0dd1fbe45ed56057457198af66803
Josh Padilla <jdp237@nau.edu>
2020-10-13 23:20:42 -0700

    added badge functionality for whichever badge is selected

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-return `
+  let badge = ""
+
+  if (data.license === "MIT License") {
+    badge = "https://img.shields.io/badge/license-MIT-green"
+  } else if (data.license === "Apache License 2.0") {
+    badge = "https://img.shields.io/badge/license-Apache%20License%202.0-blue"
+  } else if (data.license === "GNU General Public License v3.0") {
+    badge = "https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0"
+  } else if (data.license === "BSD 3") {
+    badge = "https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause"
+  } else {
+    badge = ""
+  }
+  return `
 # ${data.title}
 ![License badge](${badge})
 
